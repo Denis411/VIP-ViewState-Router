1c1085c0fe2bcbf34d84127cd0e64c39732834fd
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-10-22 13:45:21 +0700

    Add load button

--- VIP+State+Router/VIP_Module/MainScreen.swift
+++ VIP+State+Router/VIP_Module/MainScreen.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class MainScreen: UIViewController {
+final class Interactor {
+    func loadRandomImage(category: ImageCategory) {
+        
+    }
+}
 
+class MainScreen: UIViewController {
+    private let interactor = Interactor()
+    private let button: UIButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        setUIButton()
     }
 
+    private func setUIButton() {
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        button.layer.backgroundColor = UIColor.blue.cgColor
+        button.layer.cornerRadius = 10.0
+        button.setTitle("Random Image", for: .normal)
+        button.setTitle("", for: .highlighted)
+        button.addTarget(nil, action: #selector(loadImage), for: .touchUpInside)
+    }
+    
+    @objc private func loadImage() {
+        interactor.loadRandomImage(category: .city)
+    }
 
 }
 
